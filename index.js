4c98ca600a8aed4041a2c1ea3529d4566d5ff7cf
@carmen.romero <carmen.romero@sygris.com>
2024-11-04 20:13:43 +0100

    fix stock

--- index.js
+++ index.js
@@ -14,24 +14,19 @@ cartIcon.addEventListener('click', toggleCartVisibility);
 function buyProduct(product) {
   let productInCart = cart.find(item => item.id === product.id);
 
-  if (productInCart) {
-    if (1 > product.stock) {
-      alert("No quedan suficientes productos en stock");
-    } else {
+  if (product.stock > 0) {
+    if (productInCart) {
       productInCart.quantity++;
-      product.stock -= 1;
-    }
-  } else {
-    if (product.stock <= 0) {
-      alert("No quedan productos en stock");
     } else {
       cart.push({ ...product, quantity: 1 });
-      product.stock -= 1;
     }
+    product.stock -= 1;
+    updateCart(); 
+  } else {
+    alert("No quedan productos en stock");
   }
 
-  updateCart();
-  if (product.stock > 0) {
+  if (cart.length > 0) {
     shoppingCart.classList.remove('hidden');
   }
   shoppingCart.scrollIntoView({ behavior: 'smooth', block: 'start' });
@@ -43,21 +38,21 @@ function updateCart() {
 
   cart.forEach(product => {
     $rowCart.innerHTML += `
-    <div class="product-row">
-      <img class="product-img" src="${product.image}" alt="${product.name}">
-
-      <div class="product-info">
+      <div class="product-row">
+        <img class="product-img" src="${product.image}" alt="${product.name}">
+        
+        <div class="product-info">
           <p class="product-name">${product.name} </p>
           <div class="product-price">
             <p>${product.price.toFixed(2)}€ - X <span class="product-quantity">${product.quantity}</span></p> 
-              <div class="product-btns">
-                  <button class="deleteProductBtn" data-id="${product.id}">Eliminar</button>
-                  <button class="plusProductBtn" data-id="${product.id}">+</button>
-                  <button class="restProductBtn" data-id="${product.id}">-</button>
-              </div>
+            <div class="product-btns">
+              <button class="deleteProductBtn" data-id="${product.id}">Eliminar</button>
+              <button class="plusProductBtn" data-id="${product.id}">+</button>
+              <button class="restProductBtn" data-id="${product.id}">-</button>
+            </div>
           </div>
-      </div>
-    </div>`;
+        </div>
+      </div>`;
   });
 
   const total = totalPrice();
@@ -112,15 +107,16 @@ function btnEventListeners() {
 
 function restProduct(product) {
   let productInCart = cart.find(item => item.id === product.id);
+  const originalProduct = products.find(p => p.id === product.id);
 
   if (productInCart) {
     if (productInCart.quantity > 1) {
       productInCart.quantity -= 1;
-      product.stock += 1;
+      originalProduct.stock += 1;
     } else {
-      // Si solo queda una unidad, eliminarlo del carrito despues de restar
+     // Si solo queda una unidad, eliminarlo del carrito despues de restar
       cart = cart.filter(item => item.id !== product.id);
-      product.stock += 1;
+      originalProduct.stock += 1;
     }
     updateCart();
   } else {
@@ -130,28 +126,38 @@ function restProduct(product) {
 
 function sumProduct(product) {
   let productInCart = cart.find(item => item.id === product.id);
+  const originalProduct = products.find(p => p.id === product.id);
 
   if (productInCart) {
-    // Solo aumentar si hay stock
-    if (product.stock > 0) {
+       // Solo aumentar si hay stock
+    if (originalProduct.stock > 0) {
       productInCart.quantity += 1;
-      product.stock -= 1;
+      originalProduct.stock -= 1;
+      updateCart();
     } else {
       alert("No queda suficiente stock");
     }
-    updateCart();
   } else {
     alert("El producto no está en el carrito");
   }
 }
 
 function deleteProduct(product) {
+  const originalProduct = products.find(p => p.id === product.id);
   cart = cart.filter(item => item.id !== product.id);
-  product.stock += product.quantity;
+  originalProduct.stock += product.quantity;
   updateCart();
 }
 
 function emptyCart() {
+  
+  cart.forEach(item => {
+    const originalProduct = products.find(p => p.id === item.id);
+    if (originalProduct) {
+      originalProduct.stock += item.quantity;
+    }
+  });
+
   cart = [];
 
   var $rowCart = document.querySelector('.row-cart');
@@ -171,6 +177,7 @@ finalizeBtn.addEventListener('click', finalize);
 
 function finalize() {
   alert('Tu compra ha sido realizada correctamente');
+  emptyCart();
 }
 
 function renderCards(products) {
